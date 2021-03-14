47554dd4073bd8a16b93141ed7d4970015803b06
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-14 22:37:22 +0300

    fix chrome options

--- conftest.py
+++ conftest.py
@@ -6,27 +6,27 @@ from settings import DB_URL
 from framework.db.postgres.db_steps import DBSteps
 
 
-@pytest.fixture()
-def browser():
-    options = Options()
-    options.headless = os.getenv('HEADLESS', False)
-    browser = webdriver.Chrome(options=options)
-    browser.maximize_window()
-    browser.implicitly_wait(5)
-    yield browser
-    browser.quit()
-
 # @pytest.fixture()
 # def browser():
-#     chrome_options = webdriver.ChromeOptions()
-#     chrome_options.add_argument('--no-sandbox')
-#     chrome_options.add_argument('--headless')
-#     chrome_options.add_argument('--disable-gpu')
-#     browser = webdriver.Chrome(chrome_options=chrome_options)
+#     options = Options()
+#     options.headless = os.getenv('HEADLESS', False)
+#     browser = webdriver.Chrome(options=options)
 #     browser.maximize_window()
 #     browser.implicitly_wait(5)
 #     yield browser
 #     browser.quit()
+
+@pytest.fixture()
+def browser():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.maximize_window()
+    browser.implicitly_wait(5)
+    yield browser
+    browser.quit()
 
 
 # db_steps = DBSteps(DB_URL)
