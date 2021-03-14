4b0ad142046f220ae623b8d79b4079f8225a3390
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-14 21:03:55 +0300

    fix requirements

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from settings import DB_URL
 from framework.db.postgres.db_steps import DBSteps
 
+
 @pytest.fixture()
 def browser():
     chrome_options = webdriver.ChromeOptions()
@@ -14,13 +15,6 @@ def browser():
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
-
-# @pytest.fixture
-# # def browser():
-# #     browser = webdriver.Chrome()
-# #     browser.implicitly_wait(5)
-# #     yield browser
-# #     browser.quit()
 
 
 db_steps = DBSteps(DB_URL)
