0c8c57be00bebf8ec1c6cc0fab607b433ee76e00
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-14 22:00:59 +0300

    fix chrome options

--- conftest.py
+++ conftest.py
@@ -1,20 +1,32 @@
+import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from settings import DB_URL
 from framework.db.postgres.db_steps import DBSteps
 
 
 @pytest.fixture()
 def browser():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    browser = webdriver.Chrome(chrome_options=chrome_options)
+    options = Options()
+    options.headless = os.getenv('HEADLESS', False)
+    browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
+
+# @pytest.fixture()
+# def browser():
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.add_argument('--no-sandbox')
+#     chrome_options.add_argument('--headless')
+#     chrome_options.add_argument('--disable-gpu')
+#     browser = webdriver.Chrome(chrome_options=chrome_options)
+#     browser.maximize_window()
+#     browser.implicitly_wait(5)
+#     yield browser
+#     browser.quit()
 
 
 # db_steps = DBSteps(DB_URL)
