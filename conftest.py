5f8a8370191b93f8a43a02327517e87873d07c3e
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-14 20:24:22 +0300

    fix requirements

--- conftest.py
+++ conftest.py
@@ -3,13 +3,24 @@ from selenium import webdriver
 from settings import DB_URL
 from framework.db.postgres.db_steps import DBSteps
 
-
-@pytest.fixture
+@pytest.fixture()
 def browser():
-    browser = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.maximize_window()
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
+
+# @pytest.fixture
+# # def browser():
+# #     browser = webdriver.Chrome()
+# #     browser.implicitly_wait(5)
+# #     yield browser
+# #     browser.quit()
 
 
 db_steps = DBSteps(DB_URL)
