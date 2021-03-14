e03af9cb8dbb4b0edd80b8e17c5d64796994dcbf
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-14 20:47:24 +0300

    fix requirements

--- tests/db_tests/test_new_user_add_to_group.py
+++ tests/db_tests/test_new_user_add_to_group.py
@@ -1,9 +1,12 @@
+import allure
 from pages.django_pages.main_page import MainPage
 from time import sleep
 
-
+@allure.story("Open admin")
 def test_open_admin(browser):
-    main_page = MainPage(browser)
-    main_page.open_base_page()
+    with allure.step('Open main page'):
+        main_page = MainPage(browser)
+    with allure.step('Open admin page'):
+        main_page.open_base_page()
     print('ok')
     sleep(5)
